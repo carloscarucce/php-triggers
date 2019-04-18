ba35ff35d423f2121965afce9cc62f5ea297bbc6
Carlos <carloscarucce@gmail.com>
2019-04-18 09:27:20 -0300

    implements EventListener class and makes it abstract

--- src/EventListener.php
+++ src/EventListener.php
@@ -2,7 +2,42 @@
 
 namespace PhpTriggers;
 
-class EventListener
+abstract class EventListener
 {
-    //TODO create listener
+    /**
+     * @var array
+     */
+    private $eventTypes = [];
+
+    /**
+     * Action to be executed when the event is triggered.
+     *
+     * @param Event $event
+     * @param mixed ...$data
+     *
+     * @return mixed
+     */
+    abstract public function listen(Event $event, ...$data);
+
+    /**
+     * Determines what types of events this will listen to.
+     *
+     * @param string|string[] $types
+     */
+    public function listensTo($type)
+    {
+        $this->eventTypes = (array) $type;
+    }
+
+    /**
+     * Informs if the current listener can listen to $event.
+     *
+     * @param Event $event
+     *
+     * @return bool
+     */
+    final public function willListenTo(Event $event)
+    {
+        return in_array($event->getType(), $this->eventTypes);
+    }
 }
